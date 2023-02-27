9093e00f601ef441d39ae338c73d1e158bd4c53c
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-02-27 12:09:04 +0530

    Issue #ULP-93 fix: Show Hide dashboard root page

--- src/app/main-dashboard/main-dashboard.component.ts
+++ src/app/main-dashboard/main-dashboard.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-dashboard',
   templateUrl: './main-dashboard.component.html',
   styleUrls: ['./main-dashboard.component.scss']
 })
-export class MainDashboardComponent implements OnInit {
+export class MainDashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  isChildRoute = false;
+  private unsubscribe$ = new Subject<void>();
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+    this.router.events.pipe(
+      takeUntil(this.unsubscribe$),
+      filter(e => e instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.isChildRoute = !!this.activatedRoute.children.length;
+      });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
