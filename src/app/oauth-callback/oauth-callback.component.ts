0c6f416d7f114b267049b3ede75cc3235fc80b96
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-03-15 20:04:09 +0530

    Issue #ULP-93 feat: SSO Implementation

---
+++ src/app/oauth-callback/oauth-callback.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GeneralService } from '../services/general/general.service';
+
+@Component({
+  selector: 'app-oauth-callback',
+  templateUrl: './oauth-callback.component.html',
+  styleUrls: ['./oauth-callback.component.scss']
+})
+export class OauthCallbackComponent implements OnInit {
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private generalService: GeneralService
+    ) {
+    this.activatedRoute.queryParams.subscribe((params: any) => {
+      console.log("params", params);
+      if (params.code) {
+        this.getUserData(params.code);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  getUserData(code: string) {
+    const request = {
+      auth_code: code
+    }
+    this.generalService.postData('https://ulp.uniteframework.io/ulp-bff/v1/sso/digilocker/token', request).subscribe((res) => {
+      console.log("Result", res);
+    });
+  }
+}
