0303bc9358c823caaa0c0690da9d7914ccbd2d7f
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-03-18 00:29:34 +0530

    Issue #ULP-131 feat: SSO Implementation

--- src/app/oauth-callback/oauth-callback.component.ts
+++ src/app/oauth-callback/oauth-callback.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { GeneralService } from '../services/general/general.service';
+import { ToastMessageService } from '../services/toast-message/toast-message.service';
 
 @Component({
   selector: 'app-oauth-callback',
@@ -11,8 +12,10 @@ export class OauthCallbackComponent implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private generalService: GeneralService
-    ) {
+    private generalService: GeneralService,
+    private toastMessage: ToastMessageService,
+    private router: Router,
+  ) {
     this.activatedRoute.queryParams.subscribe((params: any) => {
       console.log("params", params);
       if (params.code) {
@@ -28,11 +31,33 @@ export class OauthCallbackComponent implements OnInit {
 
   getUserData(code: string) {
     const request = {
-      digiacc:"portal",
+      digiacc: "portal",
       auth_code: code
     }
-    this.generalService.postData('https://ulp.uniteframework.io/ulp-bff/v1/sso/digilocker/token', request).subscribe((res) => {
+    this.generalService.postData('https://ulp.uniteframework.io/ulp-bff/v1/sso/digilocker/token', request).subscribe((res: any) => {
       console.log("Result", res);
+
+      if (res.success) {
+        if (res.user === 'FOUND') {
+          if (res.token) {
+            localStorage.setItem('accessToken', res.token);
+          }
+
+          if (res?.userData?.length) {
+            localStorage.setItem('currentUser', JSON.stringify(res.userData[0]));
+          }
+          this.router.navigate(['/dashboard']);
+        }
+
+        if (res.user === 'NO_FOUND' && res.result) {
+          const navigationExtras: NavigationExtras = {
+            state: res.result
+          };
+          this.router.navigate(['/register'], navigationExtras)
+        }
+      } else {
+        this.toastMessage.error('', 'Error while login');
+      }
     });
   }
 }
--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgbModal, NgbModalOptions, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { GeneralService } from '../services/general/general.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastMessageService } from '../services/toast-message/toast-message.service';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-registration-form',
@@ -7,9 +13,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationFormComponent implements OnInit {
 
-  constructor() { }
+  registrationDetails: any;
+  schoolDetails: any;
+  modalRef: NgbModalRef;
+  @ViewChild('udiseLinkModal') udiseLinkModal: TemplateRef<any>;
+
+  registrationForm = new FormGroup({
+    schoolName: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
+    udiseId: new FormControl(null, [Validators.required]),
+    name: new FormControl(null, [Validators.required, Validators.minLength(2)]),
+    phone: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[0-9]{10}$')]),
+    aadharId: new FormControl(null, [Validators.required, Validators.minLength(12), Validators.maxLength(12), Validators.pattern('^[0-9]*$')]),
+    joiningdate: new FormControl(null, [Validators.required]),
+    consent: new FormControl(false, [Validators.required, Validators.requiredTrue])
+  });
+
+  constructor(
+    private readonly router: Router,
+    private readonly modalService: NgbModal,
+    private readonly generalService: GeneralService,
+    private readonly toastMessage: ToastMessageService,
+    private readonly authService: AuthService
+  ) {
+    const navigation = this.router.getCurrentNavigation();
+    this.registrationDetails = navigation.extras.state;
+  }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit() {
+    if (this.registrationDetails) {
+      if (this.registrationDetails?.name) {
+        this.registrationForm.get('name').setValue(this.registrationDetails.name);
+      }
+
+      if (this.registrationDetails?.mobile) {
+        this.registrationForm.get('phone').setValue(this.registrationDetails.mobile);
+      }
+    }
+    const options: NgbModalOptions = {
+      backdrop: 'static',
+      animation: true,
+      centered: true,
+
+    }
+    this.modalRef = this.modalService.open(this.udiseLinkModal, options);
+  }
+
+  linkUDISE() {
+    this.generalService.getData('https://ulp.uniteframework.io/ulp-bff/v1/sso/udise/verify/myschool1234', true).subscribe((res: any) => {
+      this.schoolDetails = res;
+      this.toastMessage.success('', 'Successfully Linked!');
+      this.modalRef.close();
+    });
+  }
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    if (this.registrationForm.valid) {
+
+      const payload = {
+        "digiacc": "portal",
+        "userdata": {
+          "teacher": {
+            "name": this.registrationForm.value.name,
+            "joiningdate": this.registrationForm.value.joiningdate,
+            "aadharId": this.registrationForm.value.aadharId,
+            "schoolUdise": this.schoolDetails.udiseCode,
+            "meripehchanLoginId": this.registrationDetails.meripehchanid,
+            "username": this.registrationDetails.meripehchanid,
+            "consent": "yes",
+            "consentDate": Date.now(),
+            "did": ""
+          },
+          "school": { ...this.schoolDetails, did: "" }
+        },
+        "digimpid": this.registrationDetails.meripehchanid
+      }
+
+      this.authService.ssoSignUp(payload).subscribe((res: any) => {
+        console.log("result register", res);
+        if (res.success && res.user === 'FOUND') {
+
+          if (res.token) {
+            localStorage.setItem('accessToken', res.token);
+          }
+
+          if (res?.userData?.student) {
+            localStorage.setItem('currentUser', JSON.stringify(res.userData.student));
+          }
+          this.router.navigate(['/dashboard']);
+
+          this.toastMessage.success("", "User Registered successfully!");
+
+        } else {
+          this.toastMessage.error("", res.message);
+        }
+      }, (error) => {
+        this.toastMessage.error("", error.message);
+      });
+
+    }
   }
 
 }
