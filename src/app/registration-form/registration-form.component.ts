c96fd9248bbf3102b0699300b867e4eccfe816ac
Vivek Kasture <vivek_kasture@techjoomla.com>
2023-03-18 23:36:56 +0530

    Issue #ULP-131 feat: SSO Implementation

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -5,6 +5,7 @@ import { GeneralService } from '../services/general/general.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastMessageService } from '../services/toast-message/toast-message.service';
 import { AuthService } from '../services/auth/auth.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-registration-form',
@@ -15,8 +16,13 @@ export class RegistrationFormComponent implements OnInit {
 
   registrationDetails: any;
   schoolDetails: any;
-  modalRef: NgbModalRef;
+  udiseLinkModalRef: NgbModalRef;
+  consentModalRef: NgbModalRef;
+  maxDate = new Date().toISOString().split("T")[0];
+  isDeclarationSubmitted = false;
+  userConsent = false;
   @ViewChild('udiseLinkModal') udiseLinkModal: TemplateRef<any>;
+  @ViewChild('declarationModal') declarationModal: TemplateRef<any>;
 
   registrationForm = new FormGroup({
     schoolName: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]+$')]),
@@ -24,8 +30,8 @@ export class RegistrationFormComponent implements OnInit {
     name: new FormControl(null, [Validators.required, Validators.minLength(2)]),
     phone: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern('^[0-9]{10}$')]),
     aadharId: new FormControl(null, [Validators.required, Validators.minLength(12), Validators.maxLength(12), Validators.pattern('^[0-9]*$')]),
-    joiningdate: new FormControl(null, [Validators.required]),
-    consent: new FormControl(false, [Validators.required, Validators.requiredTrue])
+    joiningdate: new FormControl(null, [Validators.required, Validators.max(Date.now())]),
+    // consent: new FormControl(false, [Validators.required, Validators.requiredTrue])
   });
 
   constructor(
@@ -33,13 +39,47 @@ export class RegistrationFormComponent implements OnInit {
     private readonly modalService: NgbModal,
     private readonly generalService: GeneralService,
     private readonly toastMessage: ToastMessageService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly location: Location
   ) {
     const navigation = this.router.getCurrentNavigation();
     this.registrationDetails = navigation.extras.state;
+    const canGoBack = !!(this.router.getCurrentNavigation()?.previousNavigation);
+
+    if (!this.registrationDetails) {
+        if (canGoBack) {
+            this.location.back();
+        } else {
+            this.router.navigate(['']);
+        }
+    }
   }
 
   ngOnInit(): void { }
+
+  get schoolName() {
+    return this.registrationForm.get('schoolName');
+  }
+
+  get udiseId() {
+    return this.registrationForm.get('udiseId');
+  }
+
+  get name() {
+    return this.registrationForm.get('name');
+  }
+
+  get aadharId() {
+    return this.registrationForm.get('aadharId');
+  }
+
+  get joiningdate() {
+    return this.registrationForm.get('joiningdate');
+  }
+
+  get phone() {
+    return this.registrationForm.get('phone');
+  }
 
   ngAfterViewInit() {
     if (this.registrationDetails) {
@@ -50,6 +90,14 @@ export class RegistrationFormComponent implements OnInit {
       if (this.registrationDetails?.mobile) {
         this.registrationForm.get('phone').setValue(this.registrationDetails.mobile);
       }
+
+      if (this.schoolDetails.udiseCode) {
+        this.registrationForm.get('udiseId').setValue(this.schoolDetails.udiseCode);
+      }
+
+      if (this.schoolDetails.schoolName) {
+        this.registrationForm.get('schoolName').setValue(this.schoolDetails.schoolName);
+      }
     }
     const options: NgbModalOptions = {
       backdrop: 'static',
@@ -57,21 +105,35 @@ export class RegistrationFormComponent implements OnInit {
       centered: true,
 
     }
-    this.modalRef = this.modalService.open(this.udiseLinkModal, options);
+    this.udiseLinkModalRef = this.modalService.open(this.udiseLinkModal, options);
   }
 
   linkUDISE() {
     this.generalService.getData('https://ulp.uniteframework.io/ulp-bff/v1/sso/udise/verify/myschool1234', true).subscribe((res: any) => {
       this.schoolDetails = res;
       this.toastMessage.success('', 'Successfully Linked!');
-      this.modalRef.close();
+      this.udiseLinkModalRef.close();
     });
+  }
+
+  submitDeclarationForm(isConfirmed: boolean) {
+    this.isDeclarationSubmitted = isConfirmed;
+    this.userConsent = isConfirmed;
+    this.consentModalRef.close()
+
+    if (isConfirmed) {
+      this.onSubmit();
+    }
   }
 
   onSubmit() {
     console.log(this.registrationForm.value);
-    if (this.registrationForm.valid) {
 
+    if (!this.isDeclarationSubmitted) {
+      this.consentModalRef = this.modalService.open(this.declarationModal, { animation: true, centered: true });
+      return;
+    }
+    if (this.registrationForm.valid) {
       const payload = {
         digiacc: "portal",
         userdata: {
